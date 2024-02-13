2f5dd4ba7cff4d646808a69b0bdbcbe564886a2f
Melihcan Fidan <melihcan.fi@gmail.com>
2024-02-13 20:51:24 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,12 +8,19 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: TextButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.
+            },
+            child: const Text('Click me'),
+          ),
         ),
       ),
     );
